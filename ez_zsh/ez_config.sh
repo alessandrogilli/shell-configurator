c82718af73adbf6186fbe6e2734bdfa60ae5c73b
california-ale <38673056+california-ale@users.noreply.github.com>
2021-05-21 16:08:12 +0200

    added virtualenv plugin

--- ez_zsh/ez_config.sh
+++ ez_zsh/ez_config.sh
@@ -2,7 +2,7 @@
 if(test -f ~/.zshrc)
 then
 	cp ~/.zshrc ~/.zshrc_bckp
-	sed 's/plugins=(git)/plugins=(git timer python)/g' ~/.zshrc_bckp > ~/.zshrc
+	sed 's/plugins=(git)/plugins=(git timer virtualenv)/g' ~/.zshrc_bckp > ~/.zshrc
 	rm ~/.zshrc_bckp
 else
 	echo "Please run \"ez_install.sh\" first."
